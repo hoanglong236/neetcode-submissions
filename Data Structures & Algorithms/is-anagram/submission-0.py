60980db759f3a1c4edd0f2f2cf1b2a7942601254
hoanglong236 <99656288+hoanglong236@users.noreply.github.com>
2026-04-29 10:46:14 +0700

    Bulk sync: 3 submissions

---
+++ Data Structures & Algorithms/is-anagram/submission-0.py
@@ -0,0 +1,10 @@
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        if len(s) != len(t):
+            return False
+        chars = [0] * 26
+        for c in s:
+            chars[ord(c) - ord('a')] += 1
+        for c in t:
+            chars[ord(c) - ord('a')] -= 1
+        return not any(chars)
