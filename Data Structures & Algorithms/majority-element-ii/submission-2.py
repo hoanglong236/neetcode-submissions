4d28484eedee33983d07463285467a248ac45b98
hoanglong236 <99656288+hoanglong236@users.noreply.github.com>
2026-05-16 13:35:36 +0700

    Add: majority-element-ii - submission-2

---
+++ Data Structures & Algorithms/majority-element-ii/submission-2.py
@@ -0,0 +1,38 @@
+from collections import Counter
+
+class Solution:
+    def majorityElement(self, nums: List[int]) -> List[int]:
+        if len(nums) <= 2:
+            return nums
+
+        p1, p1_count = None, 0
+        p2, p2_count = None, 0
+
+        for num in nums:
+            if num == p1:
+                p1_count += 1
+            elif num == p2:
+                p2_count += 1
+            else:
+                if p1_count == 0:
+                    p1, p1_count = num, 1
+                elif p2_count == 0:
+                    p2, p2_count = num, 1
+                else:
+                    p1_count -= 1
+                    p2_count -= 1
+
+        p1_count = 0
+        p2_count = 0
+        for num in nums:
+            if num == p1:
+                p1_count += 1
+            elif num == p2:
+                p2_count += 1
+
+        ans = []
+        if p1_count > len(nums) / 3:
+            ans.append(p1)
+        if p2_count > len(nums) / 3:
+            ans.append(p2)
+        return ans
