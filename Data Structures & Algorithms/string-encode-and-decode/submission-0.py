1cb249046a672868401de925ccae655854e1a762
hoanglong236 <99656288+hoanglong236@users.noreply.github.com>
2026-08-13 23:41:52 +0700

    Update: string-encode-and-decode - submission-0

--- Data Structures & Algorithms/string-encode-and-decode/submission-0.py
+++ Data Structures & Algorithms/string-encode-and-decode/submission-0.py
@@ -1,18 +1,16 @@
 class Solution:
 
     def encode(self, strs: List[str]) -> str:
-        ans = []
-        for s in strs:
-            ans.append(".".join([str(len(s)), s]))
-        return "".join(ans)
+        return ''.join((str(len(s)) + '*' + s for s in strs))
 
     def decode(self, s: str) -> List[str]:
-        ans = []
-        s_len_idx = 0
-        for i in range(len(s)):
-            if s[i] == '.':
-                s_len = int(s[s_len_idx:i])
-                ans.append(s[i + 1: i + s_len + 1])
-                s_len_idx = i + s_len + 1
-                i += s_len
-        return ans
+        res, i, size = [], 0, 0
+        while i < len(s):
+            if s[i] != '*':
+                size = size * 10 + int(s[i])
+                i += 1
+            else:
+                res.append(s[i + 1:i + size + 1])
+                i += size + 1
+                size = 0
+        return res
