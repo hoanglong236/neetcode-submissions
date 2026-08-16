b6d9c22086a7806319b69f9cd8fd2b7c295401ae
hoanglong236 <99656288+hoanglong236@users.noreply.github.com>
2026-08-16 22:11:43 +0700

    Update: sort-colors - submission-0

--- Data Structures & Algorithms/sort-colors/submission-0.py
+++ Data Structures & Algorithms/sort-colors/submission-0.py
@@ -3,11 +3,14 @@ class Solution:
         """
         Do not return anything, modify nums in-place instead.
         """
-        buckets = [0] * 3
-        for num in nums:
-            buckets[num] += 1
+        start, end = 0, len(nums) - 1
         i = 0
-        for num, count in enumerate(buckets):
-            for _ in range(count):
-                nums[i] = num
+        while i < len(nums):
+            if nums[i] == 0 and start < i:
+                nums[start], nums[i] = nums[i], nums[start]
+                start += 1
+            elif nums[i] == 2 and i < end:
+                nums[end], nums[i] = nums[i], nums[end]
+                end -= 1
+            else:
                 i += 1
