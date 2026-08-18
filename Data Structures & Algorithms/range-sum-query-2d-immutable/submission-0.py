421a0812d9928236d2f587f63254f59607234516
hoanglong236 <99656288+hoanglong236@users.noreply.github.com>
2026-08-18 22:52:13 +0700

    Update: range-sum-query-2d-immutable - submission-0

--- Data Structures & Algorithms/range-sum-query-2d-immutable/submission-0.py
+++ Data Structures & Algorithms/range-sum-query-2d-immutable/submission-0.py
@@ -1,42 +1,27 @@
 class NumMatrix:
 
     def __init__(self, matrix: List[List[int]]):
-        self.prefix_area = []
-        n, m = len(matrix), len(matrix[0])
-        self.prefix_area = [[[] for _ in range(m)] for _ in range(n)]
-
-        self.prefix_area[0][0] = matrix[0][0]
-        # Calculate the first row
-        for j in range(1, m):
-            self.prefix_area[0][j] = self.prefix_area[0][j - 1] + matrix[0][j]
-        # Calculate the first col
-        for i in range(1, n):
-            self.prefix_area[i][0] = self.prefix_area[i - 1][0] + matrix[i][0]
-        # Calculate other cells
-        for i in range(1, n):
-            for j in range(1, m):
-                self.prefix_area[i][j] = (
-                    matrix[i][j]
-                    + self.prefix_area[i][j - 1]
-                    + self.prefix_area[i - 1][j]
-                    - self.prefix_area[i - 1][j - 1]
-                )
+        m, n = len(matrix), len(matrix[0])
+        self.prefix = [[[0] for _ in range(n)] for _ in range(m)]
+        for r in range(m):
+            for c in range(n):
+                self.prefix[r][c] = matrix[r][c]
+                if r > 0:
+                    self.prefix[r][c] += self.prefix[r - 1][c]
+                if c > 0:
+                    self.prefix[r][c] += self.prefix[r][c - 1]
+                if r > 0 and c > 0:
+                    self.prefix[r][c] -= self.prefix[r - 1][c - 1]
 
     def sumRegion(self, row1: int, col1: int, row2: int, col2: int) -> int:
-        area = self.prefix_area[row2][col2]
-        if row1 == 0:
-            if col1 == 0:
-                return area
-            return area - self.prefix_area[row2][col1 - 1]
-        if col1 == 0:
-            return area - self.prefix_area[row1 - 1][col2]
-        return (
-            area
-            - self.prefix_area[row2][col1 - 1]
-            - self.prefix_area[row1 - 1][col2]
-            + self.prefix_area[row1 - 1][col1 - 1]
-        )
-
+        res = self.prefix[row2][col2]
+        if row1 > 0:
+            res -= self.prefix[row1 - 1][col2]
+        if col1 > 0:
+            res -= self.prefix[row2][col1 - 1]
+        if row1 > 0 and col1 > 0:
+            res += self.prefix[row1 - 1][col1 - 1]
+        return res
 
 # Your NumMatrix object will be instantiated and called as such:
 # obj = NumMatrix(matrix)
