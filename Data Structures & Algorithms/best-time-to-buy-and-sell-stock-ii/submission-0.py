a69aa3a9c0d282534432adeb9b54ac765d59b675
hoanglong236 <99656288+hoanglong236@users.noreply.github.com>
2026-08-24 22:18:16 +0700

    Update: best-time-to-buy-and-sell-stock-ii - submission-0

--- Data Structures & Algorithms/best-time-to-buy-and-sell-stock-ii/submission-0.py
+++ Data Structures & Algorithms/best-time-to-buy-and-sell-stock-ii/submission-0.py
@@ -1,23 +1,9 @@
 class Solution:
     def maxProfit(self, prices: List[int]) -> int:
-        profit = 0
-        buy, sell = prices[0], -1
-        i = 1
-        while i < len(prices):
-            if prices[i] > buy:
-                if prices[i] > sell:
-                    sell = prices[i]
-                else:
-                    profit += sell - buy
-                    buy = prices[i]
-                    sell = -1
-            else:
-                if sell > -1:
-                    profit += sell - buy
-                    sell = -1
-                buy = prices[i]
-            i += 1
-        if sell > -1:
-            profit += sell - buy
-        return profit
-
+        ans = 0
+        buy = prices[0]
+        for price in prices:
+            if price > buy:
+                ans += price - buy
+            buy = price
+        return ans
