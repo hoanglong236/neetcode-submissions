73f401c8aaf46a5678d6f3733f3920186aa1d86b
hoanglong236 <99656288+hoanglong236@users.noreply.github.com>
2026-08-28 23:04:51 +0700

    Update: subarray-sum-equals-k - submission-1

--- Data Structures & Algorithms/subarray-sum-equals-k/submission-1.py
+++ Data Structures & Algorithms/subarray-sum-equals-k/submission-1.py
@@ -2,17 +2,11 @@ from collections import defaultdict
 
 class Solution:
     def subarraySum(self, nums: List[int], k: int) -> int:
-        ans = 0
-        current_sum = 0
-
-        prefix_sum = defaultdict(int)
-        # Must have to count these cases:
-        # Subarrays start with index 0
-        # The element itself equals k
-        prefix_sum[0] = 1
-
+        res, running_sum = 0, 0
+        freq_sum = defaultdict(int)
+        freq_sum[0] = 1
         for num in nums:
-            current_sum += num
-            ans += prefix_sum[current_sum - k]
-            prefix_sum[current_sum] += 1
-        return ans
+            running_sum += num
+            res += freq_sum[running_sum - k]
+            freq_sum[running_sum] += 1
+        return res
