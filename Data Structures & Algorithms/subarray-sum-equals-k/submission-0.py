4bc0fde9966c2687fc5662cbd5f14b849bc344cd
hoanglong236 <99656288+hoanglong236@users.noreply.github.com>
2026-08-28 23:03:37 +0700

    Update: subarray-sum-equals-k - submission-0

--- Data Structures & Algorithms/subarray-sum-equals-k/submission-0.py
+++ Data Structures & Algorithms/subarray-sum-equals-k/submission-0.py
@@ -1,12 +1,10 @@
 class Solution:
     def subarraySum(self, nums: List[int], k: int) -> int:
-        ans = 0
-        # Initialize with {0: 1} to account for subarrays starting at index 0.
-        prefix_sum = {0: 1}
-        current_sum = 0
+        res, running_sum = 0, 0
+        freq_sum = {0: 1}
         for num in nums:
-            current_sum += num
-            if current_sum - k in prefix_sum:
-                ans += prefix_sum[current_sum - k]
-            prefix_sum[current_sum] = prefix_sum.get(current_sum, 0) + 1
-        return ans
+            running_sum += num
+            if running_sum - k in freq_sum:
+                res += freq_sum[running_sum - k]
+            freq_sum[running_sum] = freq_sum.get(running_sum, 0) + 1
+        return res
