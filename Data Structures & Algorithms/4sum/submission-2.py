bc527e7902e50be507c0fc4670aa22504d77e741
hoanglong236 <99656288+hoanglong236@users.noreply.github.com>
2026-08-30 23:11:29 +0700

    Update: 4sum - submission-2

--- Data Structures & Algorithms/4sum/submission-2.py
+++ Data Structures & Algorithms/4sum/submission-2.py
@@ -1,26 +1,28 @@
 class Solution:
     def fourSum(self, nums: List[int], target: int) -> List[List[int]]:
         nums.sort()
-        ans = []
-
+        res = []
         for i in range(len(nums) - 3):
             if i > 0 and nums[i] == nums[i - 1]:
                 continue
             for j in range(i + 1, len(nums) - 2):
                 if j > i + 1 and nums[j] == nums[j - 1]:
                     continue
-                l, r = j + 1, len(nums) - 1
-                while l < r:
-                    total = nums[i] + nums[j] + nums[l] + nums[r]
-                    if total == target:
-                        ans.append([nums[i] , nums[j] , nums[l] , nums[r]])                        
-                        while l < r and nums[l] == nums[l + 1] and nums[r] == nums[r - 1]:
-                            l += 1
-                            r -= 1
-                        l += 1
-                        r -= 1
-                    elif total > target:
-                        r -= 1
+                left, right = j + 1, len(nums) - 1
+                while left < right:
+                    if left > j + 1 and nums[left] == nums[left - 1]:
+                        left += 1
+                        continue
+                    if right + 1 < len(nums) and nums[right] == nums[right + 1]:
+                        right -= 1
+                        continue
+                    val = nums[i] + nums[j] + nums[left] + nums[right]
+                    if val < target:
+                        left += 1
+                    elif val > target:
+                        right -= 1
                     else:
-                        l += 1
-        return ans
+                        res.append([nums[i], nums[j], nums[left], nums[right]])
+                        left += 1
+                        right -= 1
+        return res
