ee9f597d64875e0dfd7fc0d28fa003d19a9c7e7f
hoanglong236 <99656288+hoanglong236@users.noreply.github.com>
2026-09-02 00:49:46 +0700

    Update: search-insert-position - submission-1

--- Data Structures & Algorithms/search-insert-position/submission-1.py
+++ Data Structures & Algorithms/search-insert-position/submission-1.py
@@ -1,12 +1,15 @@
 class Solution:
     def searchInsert(self, nums: List[int], target: int) -> int:
-        left, right = 0, len(nums)
-        while left < right and left < len(nums):
+        if target <= nums[0]:
+            return 0
+        if target > nums[-1]:
+            return len(nums)
+
+        left, right = 0, len(nums) - 1
+        while left < right:
             mid = (left + right) // 2
-            if nums[mid] > target:
-                right = mid
-            elif nums[mid] < target:
+            if nums[mid] < target:
                 left = mid + 1
             else:
-                return mid
+                right = mid
         return left
