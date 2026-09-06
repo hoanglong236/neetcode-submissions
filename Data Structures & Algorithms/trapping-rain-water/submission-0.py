da77149ae80764008469af45234669eaf8aa2191
hoanglong236 <99656288+hoanglong236@users.noreply.github.com>
2026-09-06 15:58:21 +0700

    Update: trapping-rain-water - submission-0

--- Data Structures & Algorithms/trapping-rain-water/submission-0.py
+++ Data Structures & Algorithms/trapping-rain-water/submission-0.py
@@ -1,9 +1,15 @@
 class Solution:
     def trap(self, height: List[int]) -> int:
-        n = len(height)
-        area = [0] * n
-
-        for i, h in enumerate(height):
-            area[i] = max(min(max([0, *height[:i]]), max([0, *height[i+1:]])) - h, 0)
-
-        return sum(area)
+        res = 0
+        left, right = 0, len(height) - 1
+        left_candidate, right_candidate = 0, 0
+        while left < right:
+            if height[left] <= height[right]:
+                left_candidate = max(left_candidate, height[left])
+                res += left_candidate - height[left]
+                left += 1
+            else:
+                right_candidate = max(right_candidate, height[right])
+                res += right_candidate - height[right]
+                right -= 1
+        return res
