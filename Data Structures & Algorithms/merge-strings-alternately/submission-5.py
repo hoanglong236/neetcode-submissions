b48f7a4aeef17ad479f90d49cd16e2eab53e992b
hoanglong236 <99656288+hoanglong236@users.noreply.github.com>
2026-09-09 00:04:57 +0700

    Add: merge-strings-alternately - submission-5

---
+++ Data Structures & Algorithms/merge-strings-alternately/submission-5.py
@@ -0,0 +1,5 @@
+from itertools import zip_longest
+
+class Solution:
+    def mergeAlternately(self, word1: str, word2: str) -> str:
+        return ''.join((x[0] + x[1] for x in zip_longest(word1, word2, fillvalue='')))
