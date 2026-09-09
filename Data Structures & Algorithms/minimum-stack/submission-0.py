26ac03f98c2278eb31a4e0fb647e09ca82ae2f1d
hoanglong236 <99656288+hoanglong236@users.noreply.github.com>
2026-09-09 23:34:58 +0700

    Update: minimum-stack - submission-0

--- Data Structures & Algorithms/minimum-stack/submission-0.py
+++ Data Structures & Algorithms/minimum-stack/submission-0.py
@@ -2,15 +2,21 @@ class MinStack:
 
     def __init__(self):
         self.stack = []
+        self.min_stack = []
 
     def push(self, val: int) -> None:
         self.stack.append(val)
+        min_val = val
+        if self.min_stack:
+            min_val = min(min_val, self.min_stack[-1])
+        self.min_stack.append(min_val)
 
     def pop(self) -> None:
         self.stack.pop()
+        self.min_stack.pop()
 
     def top(self) -> int:
         return self.stack[-1]
 
     def getMin(self) -> int:
-        return min(self.stack)
+        return self.min_stack[-1]
