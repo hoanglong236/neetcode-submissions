0ffa00476589ed724b133be6f584be686d48f361
hoanglong236 <99656288+hoanglong236@users.noreply.github.com>
2026-09-26 11:52:07 +0700

    Add: find-in-mountain-array - submission-0

---
+++ Data Structures & Algorithms/find-in-mountain-array/submission-0.py
@@ -0,0 +1,57 @@
+class Solution:
+    def findInMountainArray(self, target: int, mountainArr: 'MountainArray') -> int:
+        n = mountainArr.length()
+        heights = {}
+
+        def cacheHeight(idx):
+            if idx not in heights:
+                h = mountainArr.get(idx)
+                heights[idx] = h
+                return h
+            return heights[idx]
+
+        def findMountainIndex():
+            left, right = 0, n - 1
+            while left < right:
+                mid = (left + right) // 2
+                if mid == n - 1 or mid == 0:
+                    return -1
+                height = cacheHeight(mid)
+                next_height = cacheHeight(mid + 1)
+                if height > next_height:
+                    prev_height = cacheHeight(mid - 1)
+                    if height > prev_height:
+                        return mid
+                    else:
+                        right = mid - 1
+                else:
+                    left = mid + 1
+            return left
+
+        def binarySearchASC(left, right):
+            while left <= right:
+                mid = (left + right) // 2
+                height = heights[mid] if mid in heights else mountainArr.get(mid)
+                if height == target:
+                    return mid
+                if height < target:
+                    left = mid + 1
+                else:
+                    right = mid - 1
+            return -1
+
+        def binarySearchDESC(left, right):
+            while left <= right:
+                mid = (left + right) // 2
+                height = heights[mid] if mid in heights else mountainArr.get(mid)
+                if height == target:
+                    return mid
+                if height < target:
+                    right = mid - 1
+                else:
+                    left = mid + 1
+            return -1
+
+        mountainIdx = findMountainIndex()
+        res = binarySearchASC(0, mountainIdx)
+        return res if res != -1 else binarySearchDESC(mountainIdx, n - 1)
